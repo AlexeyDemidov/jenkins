d0ed764ad1b67f4a360e2e232f6456f7f764ab3d
Alex L. Demidov <alexeydemidov@gmail.com>
2014-04-23 09:40:01 +0400

    skip creating jenkins user and group

--- recipes/master.rb
+++ recipes/master.rb
@@ -27,22 +27,22 @@
 #
 
 # Create the Jenkins user
-user node['jenkins']['master']['user'] do
-  home node['jenkins']['master']['home']
-end
+#user node['jenkins']['master']['user'] do
+#  home node['jenkins']['master']['home']
+#end
 
 # Create the Jenkins group
-group node['jenkins']['master']['group'] do
-  members node['jenkins']['master']['user']
-end
+#group node['jenkins']['master']['group'] do
+#  members node['jenkins']['master']['user']
+#end
 
 # Create the home directory
-directory node['jenkins']['master']['home'] do
-  owner     node['jenkins']['master']['user']
-  group     node['jenkins']['master']['group']
-  mode      '0755'
-  recursive true
-end
+#directory node['jenkins']['master']['home'] do
+#  owner     node['jenkins']['master']['user']
+#  group     node['jenkins']['master']['group']
+#  mode      '0755'
+#  recursive true
+#end
 
 # Create the log directory
 directory node['jenkins']['master']['log_directory'] do
